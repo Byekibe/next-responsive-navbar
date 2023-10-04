27ac015b144de0a8e3fe9f69fbebaec4e2096767
Byekibe <kibetpeter95@gmail.com>
2023-10-04 12:29:43 +0300

    responsive navbar logo change

--- src/components/navbar2.jsx/NavbarTwo.jsx
+++ src/components/navbar2.jsx/NavbarTwo.jsx
@@ -16,7 +16,7 @@ const NavbarTwo = () => {
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link href="/" className="flex items-center">
-                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
+                {/* <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" /> */}
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Petro</span>
             </Link>
             {/* <ModalToggler /> */}
